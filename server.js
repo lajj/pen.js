44e652ebc11b014b2ac2c20114cb387a35ef9ba4
Lukars <lukas.siebeck@gmx.de>
2015-06-25 14:41:15 +0100

    api connection working with generic handler

--- server.js
+++ server.js
@@ -1,59 +1,34 @@
 var express = require('express');
 var app = express();
-var https = require('https');
-var request = require('request');
-var io = require('socket.io');
+var fs = require('fs');
+var methods = require('./github.js');
 
-function getFile (callback){
-	var url = 'https://api.github.com/repos/Lajj/pen.js/contents/log.md?ref=notify';
-	var body = '';
-	var result;
-	var headers= {'User-Agent':'pen.js',
-					'Content-Type': 'application/json'};
-
-	var options = {
-		headers: headers,
-		url: url
-	};
-    request.get(options, function (error, response, body){
-        var returnString="";
-      	if(!error && response.statusCode == 200){
-      		var b64string = JSON.parse(body);
-			    var buf = new Buffer(b64string.content, 'base64');
-      		var obj = buf.toString().replace(/time/g, "timestamp");
-      		var objArray = obj.split('\n');
-      		for(var i=0;i<objArray.length;i++){
-      			try{
-              var parsedSingleObj = JSON.parse(objArray[i]);        
-		 				  if(parsedSingleObj.author){
-  		 					var convertedTime = new Date(parseInt(parsedSingleObj.timestamp)*1000);
-                var timeString = convertedTime.toGMTString();
-     	      		returnString += "<li><h2>" + parsedSingleObj.author + "</h2> <span class='time'>" + timeString + "</span><p> " + parsedSingleObj.file + " : " + parsedSingleObj.message + "</p><p> " + parsedSingleObj.SHA + "</p></li>";
-   	      	  }  
-				    }catch(e){
-				        //console.log(e);
-				    }
-      		}
-
-      	}
-      	else if(error || response.statusCode !== 200){      		
-      		console.log(error || response.statusCode);
-		    }
-
-      callback(returnString);
-
-    }); 
-//return returnString;
-}
 
 app.get('/', function (req, res) {
-  getFile(function(string){
-     res.send(string);
+      res.redirect('/index.html');
   });
+
+app.get('/src/*', function (req, res) {
+  var ext = req.url.split('.')[1] || 'text';
+  res.writeHead(200, {'Content-Type' : 'text/' + ext});
+  res.write(fs.readFileSync(__dirname + req.url));
+  res.end();
+})
+
+app.get('/index.html', function (req, res) {
+
+    methods(function(string){
+
+       fs.readFile(__dirname + "/index.html", function (err,data){
+        //console.log(data.toString());
+        var assembleHTML = data.toString().replace("<li></li>" , string);
+        res.send(assembleHTML);
+       });
+    });
 });
 
 app.get('/notify', function (req, res) {
-  	var stuff = getFile();
+  	var stuff = methods();
   	res.send(stuff);
 });
 
@@ -62,6 +37,6 @@ var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Penjs listening at http://%s:%s', host, port);
 
 });
